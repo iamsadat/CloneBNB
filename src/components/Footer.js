54ace12ada161cd0144db013111d730ef8187e6c
Sadat <100314833+sxdatt@users.noreply.github.com>
2023-05-04 03:59:09 +0530

    Updated Original tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <p className="copyright">© 2023 CloneBNB Inc.</p>
-            <a href="https://www.airbnb.co.in/" target="_blank" ><p className="contact"> © Original</p> </a>
+            <a href="https://www.airbnb.co.in/" target="_blank" className="original" ><p> © Original</p> </a>
         </div>
     )
 }
